a03a48e7f96235c807d01fbbb16a887ec460dccf
emzhran <zachran750@gmail.com>
2025-03-18 21:48:38 +0700

    menambahkan fungsi untuk tambah data

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -14,6 +14,19 @@ class _FormPageState extends State<FormPage> {
   String? _dateError;
   List<Map<String, dynamic>> listTugas = [];
 
+  void addData() {
+    setState(() {
+      listTugas.add({
+        'task': _taskController.text,
+        'done': false,
+        'deadline': _selectedDate,
+      });
+      _taskController.clear();
+      _selectedDate = null;
+      _dateError = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
